097d63bae59a10d9a3a2faf8c5e4d08f89274f8f
Daitarus <38458173+Daitarus@users.noreply.github.com>
2022-01-09 17:37:41 +0300

    os

--- AllPing.py
+++ AllPing.py
@@ -2,6 +2,8 @@ import os
 host = "192.168.1.1"
 if os.name=="nt":
     response = os.system("ping -n 1 " + host)
+else:
+    response = os.system("ping -c 1 " + host)
 
 if response == 0:
   print(host + ' is up!')
